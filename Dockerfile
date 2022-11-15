a89bb618bd53151b923ee9c6d4155a32e46c25de
Romain Leneveu <leneveu.romain@gmail.com>
2022-11-15 16:44:09 +0100

    jwt test

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM phpswoole/swoole:php8.1-alpine
 WORKDIR /var/www
 RUN docker-php-ext-install mysqli
 RUN mkdir public &&\
-    mkdir app
+    mkdir app &&\
+    mkdir app/jwt
 COPY /server/server.php ./
 COPY /public ./public
 COPY /app ./app
+COPY /vendor/firebase/php-jwt/src ./app/jwt
 COPY /ressources/fonts ./public/assets/fonts
 
 CMD [ "php", "server.php"]
--- server/server.php
+++ server/server.php
@@ -7,6 +7,13 @@ $dbrequest = __DIR__ . "/app/model/dbrequest.php";
 $http = __DIR__ . "/app/model/http.php";
 $websocket = __DIR__ . "/app/model/websocket.php";
 $login = __DIR__ . "/app/model/login.php";
+$jwt_jwt = __DIR__ . "/app/jwt/JWT.php";
+$jwt_jwk = __DIR__ . "/app/jwt/JWK.php";
+$jwt_key = __DIR__ . "/app/jwt/Key.php";
+$jwt_before = __DIR__ . "/app/jwt/BeforeValidException.php";
+$jwt_cached = __DIR__ . "/app/jwt/CachedKeySet.php";
+$jwt_expired = __DIR__ . "/app/jwt/ExpiredException.php";
+$jwt_signature = __DIR__ . "/app/jwt/SignatureInvalidException.php";
 // $chat = __DIR__ . "/app/chat/chat.php";
 // $calendar = __DIR__ . "/app/calendar/calendar.php";
 // $caldav = __DIR__ . "/app/simplecaldav/SimpleCalDAVClient.php";
@@ -21,6 +28,13 @@ foreach ([
     // $chat,
     // $calendar,
     // $caldav,
+    $jwt_jwt,
+    $jwt_jwk,
+    $jwt_key,
+    $jwt_before,
+    $jwt_cached,
+    $jwt_expired,
+    $jwt_signature,
 ] as $value) {
     require_once $value;
     unset($value);
@@ -37,6 +51,8 @@ use Swoole\WebSocket\Server;
 use Swoole\Http\Request;
 use Swoole\Http\Response;
 use Swoole\WebSocket\Frame;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use bopdev\DBRequest;
 
 class FWServer
@@ -297,6 +313,52 @@ class FWServer
             // ]);
             // var_dump(empty($test));
         } else print('!!!! No db connection. !!!!' . PHP_EOL);
+        $privateKey = <<<EOD
+-----BEGIN RSA PRIVATE KEY-----
+MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+-----END RSA PRIVATE KEY-----
+EOD;
+
+        $publicKey = <<<EOD
+-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
+4i2p/llLCrEeQhta5kaQu/RnvuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t
+0tyazyZ8JXw+KgXTxldMPEL95+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4
+ehde/zUxo6UvS7UrBQIDAQAB
+-----END PUBLIC KEY-----
+EOD;
+
+        $payload = [
+            'iss' => 'example.org',
+            'aud' => 'example.com',
+            'iat' => 1356999524,
+            'nbf' => 1357000000
+        ];
+
+        $jwt = JWT::encode($payload, $privateKey, 'RS256');
+        echo "Encode:\n" . print_r($jwt, true) . "\n";
+
+        $decoded = JWT::decode($jwt, new Key($publicKey, 'RS256'));
+
+        /*
+ NOTE: This will now be an object instead of an associative array. To get
+ an associative array, you will need to cast it as such:
+*/
+
+        $decoded_array = (array) $decoded;
+        echo "Decode:\n" . print_r($decoded_array, true) . "\n";
     }
     public function onWorkStart($serv, $worker_id)
     {
